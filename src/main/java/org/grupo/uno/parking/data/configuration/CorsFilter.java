51a37632455d0549a140938d196aaa2d50a0be1a
carlosaltan18 <carlosaltan850@gmail.com>
2024-10-11 12:03:28 -0600

    Patch al cors

--- src/main/java/org/grupo/uno/parking/data/configuration/CorsFilter.java
+++ src/main/java/org/grupo/uno/parking/data/configuration/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
 
