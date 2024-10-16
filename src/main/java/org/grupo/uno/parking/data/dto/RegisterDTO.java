b3bd1af09d8436a66b012891439ff6909c6fe1b3
carlosaltan18 <carlosaltan850@gmail.com>
2024-10-16 12:12:33 -0600

    Cambio en el dto de register para la fecha

--- src/main/java/org/grupo/uno/parking/data/dto/RegisterDTO.java
+++ src/main/java/org/grupo/uno/parking/data/dto/RegisterDTO.java
@@ -1,5 +1,6 @@
 package org.grupo.uno.parking.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,8 +20,9 @@ public class RegisterDTO implements Serializable {
     private long registerId;
     private String plate;
     private boolean status;
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endDate;
 
     private long parkingId;
